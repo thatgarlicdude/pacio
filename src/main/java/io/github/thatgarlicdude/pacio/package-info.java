54808b1395e12ed61e555063ce54e39292def559
thatgarlicdude <garlicdude@proton.me>
2026-01-07 21:10:26 -0500

    Worked on package-info.java on the pacio package.

--- src/main/java/io/github/thatgarlicdude/pacio/package-info.java
+++ src/main/java/io/github/thatgarlicdude/pacio/package-info.java
@@ -14,5 +14,21 @@
  * limitations under the License.
  */
 
-/**A package from the PacIO library.*/
+/**
+ * <p>PacIO is a Java library used for accessing and modifying the
+ * <i>Pac-Man</i> ROM set, as well as the ROM files within it.</p>
+ * 
+ * <p>This is the main package of the library, solely featuring the main
+ * class, {@link PacIO}. This class can create core objects, such as
+ * {@link PacFile} and {@link PacDirectory}, which are used to modify
+ * the ROM files from disk. This was made to ease the accessibility for
+ * the library.</p>
+ * 
+ * <p>In addition to this package, it also features the file package
+ * called {@link io.github.thatgarlicdude.pacio.file}. That package
+ * handles generic file reading and writing, as well as directory opening
+ * and saving.</p>
+ * 
+ * @author GarlicDude
+ */
 package io.github.thatgarlicdude.pacio;
