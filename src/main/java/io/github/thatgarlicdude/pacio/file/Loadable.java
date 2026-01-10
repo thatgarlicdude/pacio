52b5afb2b76161756d5958969c0257052d2e34bb
thatgarlicdude <garlicdude@proton.me>
2026-01-10 14:35:53 -0500

    Reworked on Loadable.java.

--- src/main/java/io/github/thatgarlicdude/pacio/file/Loadable.java
+++ src/main/java/io/github/thatgarlicdude/pacio/file/Loadable.java
@@ -18,24 +18,44 @@ package io.github.thatgarlicdude.pacio.file;
 
 import java.io.IOException;
 
-/*
- * TODO: Rework on the description for the interface and and elaborate
- * on the methods.
- */
-
 /**
- * <p>An interface used for loading and unloading something from
- * memory.</p>
+ * An interface used for loading file objects to memory by "opening"
+ * them from the system's disk, and "closing" them, freeing the
+ * memory.
+ * 
+ * <p>Classes that implement this interface should make use of the
+ * {@link #open()} and {@link #close()} methods, as they access the
+ * system's disk and memory. {@link #open} will load contents from the
+ * system's disk to memory, and {@link #close} will free contents from the
+ * system's memory.</p>
+ * 
+ * <p>The classes that implement the interface are {@link PacObject},
+ * {@link PacFile}, and {@link PacDirectory}. Each of these classes handle
+ * the {@link #open()} and {@link #close()} methods differently.
+ * {@link PacObject} keeps them abstract, {@link PacFile} will load the
+ * file's bytes to memory, and {@link PacDirectory} loads the directory's
+ * entries to memory.</p>
  */
 public interface Loadable {
 	
 	/**
-	 * <p>Opens something from memory.</p>
+	 * Opens a file object from the system's disk, loading its
+	 * contents to memory.
+	 * 
+	 * <p>This method should only work when a file path is valid, hence
+	 * why this method throws the {@link IOException}. If the file path
+	 * is invalid, then it'll throw the exception instead.</p>
+	 * 
+	 * @throws IOException if reading from disk fails.
 	 */
-	public abstract void open() throws IOException;
+	void open() throws IOException;
 	
 	/**
-	 * <p>Closes something from memory.</p>
+	 * Closes a file object, freeing its contents from memory.
+	 * 
+	 * <p>Classes that implement this interface can call this method as
+	 * many times, even if the file object was already closed and freed
+	 * from memory.</p>
 	 */
-	public abstract void close();
+	void close();
 }
