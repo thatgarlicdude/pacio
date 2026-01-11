c1fd0c7910b20f2fb2db64cefe2a3d84c72dedde
thatgarlicdude <garlicdude@proton.me>
2026-01-11 13:56:16 -0500

    Reworked on the file package's info.

--- src/main/java/io/github/thatgarlicdude/pacio/file/package-info.java
+++ src/main/java/io/github/thatgarlicdude/pacio/file/package-info.java
@@ -14,13 +14,21 @@
  * limitations under the License.
  */
 
-/* 
- * TODO: Elaborate more onto the package description, like in the pacio
- * package.
- */
-
 /**
- * <p>The file package of the PacIO library. It handles generic IO
- * handling to the system's disk.</p>
+ * The file package of the PacIO library. It handles generic IO
+ * handling to the system's disk.
+ * 
+ * <p>This package features two key classes:
+ * {@link io.github.thatgarlicdude.pacio.file.PacFile}, and
+ * {@link io.github.thatgarlicdude.pacio.file.PacDirectory}.
+ * PacFile is a class that is treated like a regular file in the system's
+ * disk, while PacDirectory is treated like a directory. These classes can
+ * be opened and closed via the
+ * {@link io.github.thatgarlicdude.pacio.file.Loadable} interface, and
+ * saved via the {@link io.github.thatgarlicdude.pacio.file.Savable}
+ * interface. Those classes are used to modify any file within the
+ * <i>Pac-Man</i> ROM set, and not just ROMs themselves.</p>
+ * 
+ * @author GarlicDude
  */
 package io.github.thatgarlicdude.pacio.file;
