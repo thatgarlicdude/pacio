123d58e3501b2b28b552b7dcade7b8b20e669c9b
thatgarlicdude <garlicdude@proton.me>
2026-01-12 16:30:50 -0500

    Reworked on PacFile.java.

--- src/main/java/io/github/thatgarlicdude/pacio/file/PacFile.java
+++ src/main/java/io/github/thatgarlicdude/pacio/file/PacFile.java
@@ -23,23 +23,62 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 
-/*
- * TODO: Work on the method descriptions and rework the class description.
- * Compact the code.
- */
-
 /**
- * <p>A class that represents a file in a file system.</p>
+ * A class that represents a file in a file system.
+ * 
+ * <p>This class extends from {@link PacObject}, keeping its file path
+ * variable and the methods related to it. But because this class is
+ * treated like a regular file in the file system, it features new
+ * variables and methods that are unique to it and is related to files
+ * in the system's disk.</p>
+ * 
+ * <p>Most notably, this class features the byte array called
+ * {@link #data}. It acts like the file's bytes from the system's disk, and
+ * can be loaded to memory using {@link #open()} and saved back to the
+ * system's disk using {@link #save()}. Additionally, it can be read by
+ * calling the {@link #read(int)}, {@link #read(int, int)}, and
+ * {@link #readAll()} methods. Lastly, it can be written by calling the
+ * {@link #write(int, byte)} and {@link #write(int, byte[])} methods.</p>
+ * 
+ * <p>{@link PacFile} also features the from methods, {@link #from(Path)},
+ * {@link #from(URI)}, and {@link #from(String)}. Those methods are used
+ * to automatically open the {@link PacFile} without needing to write extra
+ * lines of code.</p>
+ * 
+ * @author GarlicDude
  */
 public final class PacFile extends PacObject implements Loadable, Savable {
 	
 	/**
-	 * <p>The byte data inside the file.</p>
+	 * The byte data inside the file.
+	 * 
+	 * <p>This variable is null by default. When the byte array is null,
+	 * it indicates that the {@link PacFile} is closed via the
+	 * {@link #close()} method. On the contrary, when the byte array is
+	 * loaded, the {@link PacFile} is opened via the {@link #open()}
+	 * method.</p>
+	 * 
+	 * <p>Additionally, this variable can only be accessed through the
+	 * {@link #read(int)} and {@link #write(int, byte)} methods. The
+	 * {@link #read(int)} and {@link #read(int, int)} methods, based on
+	 * their names, can read the byte array. On the other hand,
+	 * {@link #write(int, byte)}, and {@link #write(int, byte[])} methods
+	 * can write the bytes inside the array.</p>
+	 * 
+	 * <p>Lastly, these bytes can be saved onto the file in the system's
+	 * disk via {@link #save()}. By saving, they overwrite contents from
+	 * the data array to the file.</p>
 	 */
 	private byte[] data = null;
 	
 	/**
-	 * <p>Opens the file.</p>
+	 * Opens the file from the system's disk.
+	 * 
+	 * <p>This will load the bytes inside the file to the class's byte
+	 * data, {@link #data}. It can only work if the class's {@link #path}
+	 * is valid. Otherwise, it will throw the {@link IOException}.</p>
+	 * 
+	 * @throws IOException if reading from disk fails.
 	 */
 	@Override
 	public final void open() throws IOException {
@@ -49,12 +88,17 @@ public final class PacFile extends PacObject implements Loadable, Savable {
 	}
 	
 	/**
-	 * <p>Closes the file.</p>
+	 * Closes the file, freeing its {@link #data} from memory.
+	 * 
+	 * <p>When this method is called, it will clear every byte from
+	 * {@link #data} with zeroes, and then nullify the array, clearing
+	 * it from memory.</p>
 	 */
 	@Override
 	public final void close() {
 		// Stop if the data array is already closed.
 		if (data == null) return;
+		// Clear every byte inside the array with zeroes.
 		for (int index = 0; index < data.length; index++) {
 			data[index] = 0;
 		}
@@ -62,7 +106,13 @@ public final class PacFile extends PacObject implements Loadable, Savable {
 	}
 	
 	/**
-	 * <p>Saves the file.</p>
+	 * Saves the file from the system's disk by overwriting it with the
+	 * contents from {@link #data}.
+	 * 
+	 * <p>This method only works when the {@link #path} is valid.
+	 * Otherwise, it will throw the {@link IOException} instead.</p>
+	 * 
+	 * @throws IOException if writing from disk fails.
 	 */
 	@Override
 	public final void save() throws IOException {
@@ -72,14 +122,29 @@ public final class PacFile extends PacObject implements Loadable, Savable {
 	}
 	
 	/**
-	 * <p>Reads a single byte in the data array.</p>
+	 * Reads a single byte in the class's {@link #data}.
+	 * 
+	 * <p>If the {@code offset} is out of bounds, it will return 0
+	 * instead.</p>
+	 * 
+	 * @param offset the index for the class's {@link #data}.
+	 * @return the byte that was read from {@link #data}.
 	 */
 	public final byte read(final int offset) {
+		if (offset < 0 || offset >= data.length) return 0;
 		return data[offset];
 	}
 	
 	/**
-	 * <p>Reads multiple bytes in the data array.</p>
+	 * Reads multiple bytes in the class's {@link #data}.
+	 * 
+	 * <p>Like {@link #read(int)}, it will return 0 if the {@code offset}
+	 * is out of bounds. The same happens when the {@code size} is not
+	 * within the boundaries of {@link #data}.</p>
+	 * 
+	 * @param offset the index for the class's {@link #data}.
+	 * @param size the amount of bytes that will be read.
+	 * @return the bytes that were read from {@link #data}.
 	 */
 	public final byte[] read(final int offset, final int size) {
 		byte[] data = new byte[size];
@@ -90,7 +155,13 @@ public final class PacFile extends PacObject implements Loadable, Savable {
 	}
 	
 	/**
-	 * <p>Reads all the bytes in the data array.</p>
+	 * Reads all the bytes in the class's {@link #data}.
+	 * 
+	 * <p>This does not return the pointer of the {@link #data}, but rather
+	 * returns a copy of it. This was done for safety reasons, as no
+	 * program should tamper this variable.</p>
+	 * 
+	 * @return the bytes that were read from {@link #data}.
 	 */
 	public final byte[] readAll() {
 		int size = data.length;
@@ -100,14 +171,29 @@ public final class PacFile extends PacObject implements Loadable, Savable {
 	}
 	
 	/**
-	 * <p>Writes a single byte in the data array.</p>
+	 * Writes a single byte in the class's {@link #data}.
+	 * 
+	 * <p>If the {@code offset} is out of bounds, it will not do
+	 * anything, and continue like normal. The {@link #data} won't be
+	 * affected if this happens.</p>
+	 * 
+	 * @param offset the index for the class's {@link #data}.
+	 * @param b the byte that will get written in {@link #data}.
 	 */
 	public final void write(final int offset, final byte b) {
+		if (offset < 0 || offset >= data.length) return;
 		data[offset] = b;
 	}
 	
 	/**
-	 * <p>Writes multiple bytes in the data array.</p>
+	 * Writes multiple bytes in the class's {@link #data}.
+	 * 
+	 * <p>Like {@link #write(int, byte)}, it will not do anything if
+	 * {@code offset} is out of bounds. This also happens when the method
+	 * tries to write bytes outside of {@link #data}.</p>
+	 * 
+	 * @param offset the index for the class's {@link #data}.
+	 * @param bytes the bytes that will be written to {@link #data}.
 	 */
 	public final void write(final int offset, final byte[] bytes) {
 		int size = bytes.length;
@@ -117,35 +203,62 @@ public final class PacFile extends PacObject implements Loadable, Savable {
 	}
 	
 	/**
-	 * <p>Creates a new PacFile using a Path.</p>
+	 * Creates a new PacFile using a file path, and automatically opens it
+	 * to memory.
+	 * 
+	 * @param path the file path to the file in the system's disk.
+	 * @return a PacFile that's automatically opened.
+	 * @throws IOException if reading from disk fails.
 	 */
 	public static PacFile from(final Path path) throws IOException {
-		PacFile rom = new PacFile(path);
-		return rom;
+		PacFile file = new PacFile(path);
+		file.open();
+		return file;
 	}
 	
 	/**
-	 * <p>Creates a new PacFile using a URI.</p>
+	 * Creates a new PacFile using a URI reference of the file path, and
+	 * automatically opens it to memory.
+	 * 
+	 * @param pathURI the URI reference of the file path.
+	 * @return a PacFile that's automatically opened.
+	 * @throws IOException if reading from disk fails.
 	 */
 	public static PacFile from(final URI pathURI) throws IOException {
 		Path path = Paths.get(pathURI);
-		PacFile rom = from(path);
-		return rom;
+		PacFile file = from(path);
+		return file;
 	}
 	
 	/**
-	 * <p>Creates a new PacFile using a string.</p>
+	 * Creates a new PacFile using a string of the file path, and
+	 * automatically opens it to memory.
+	 * 
+	 * @param pathString the string of the file path.
+	 * @return a PacFile that's automatically opened.
+	 * @throws IOException if reading from disk fails.
 	 */
-	public static PacFile from(final String pathString) throws IOException {
+	public static PacFile from(final String pathString)
+			throws IOException {
 		Path path = Paths.get(pathString);
-		PacFile rom = from(path);
-		return rom;
+		PacFile file = from(path);
+		return file;
 	}
 	
 	/**
-	 * <p>Constructs an instance of this class.</p>
+	 * Creates a new {@link PacFile} using a file path.
+	 * 
+	 * <p>This does not automatically open the {@link PacFile} when
+	 * created. If the {@link PacFile} is created and needs to be opened,
+	 * calling {@link #open()} from the object will do the trick.</p>
+	 * 
+	 * <p>The sole parameter, {@code path}, cannot be null. Otherwise, it
+	 * will throw the {@link IllegalArgumentException} if this happens.</p>
+	 * 
+	 * @param path the file path to the file object in the system's disk.
+	 * @throws IllegalArgumentException if {@code path} is null.
 	 */
-	protected PacFile(final Path path) {
+	public PacFile(final Path path) {
 		super(path);
 	}
 }
