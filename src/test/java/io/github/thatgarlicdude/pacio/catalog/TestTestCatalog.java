a93fab4b9cce14769c1ecdb08701e3e316d79960
thatgarlicdude <garlicdudedev@gmail.com>
2026-04-23 08:20:35 -0400

    Added TestTestCatalog.java

---
+++ src/test/java/io/github/thatgarlicdude/pacio/catalog/TestTestCatalog.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2026 GarlicDude
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.thatgarlicdude.pacio.catalog;
+
+/**
+ * A test class of the TestCatalog class.
+ * 
+ * @version 1.0.0
+ * @author GarlicDude
+ */
+public final class TestTestCatalog extends TestPacCatalog {
+	
+	/**
+	 * The test catalog used for testing purposes.
+	 */
+	private static final TestCatalog pacCatalog = new TestCatalog();
+	
+	/**
+	 * Constructs an instance of the test class.
+	 */
+	public TestTestCatalog() {
+		super(pacCatalog);
+	}
+}
