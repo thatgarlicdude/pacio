99e40bddb4d3298af41db2a68678236da2e84781
thatgarlicdude <garlicdudedev@gmail.com>
2026-04-27 19:41:06 -0400

    Added code to PacROMSetSaver.java

--- src/main/java/io/github/thatgarlicdude/pacio/io/PacROMSetSaver.java
+++ src/main/java/io/github/thatgarlicdude/pacio/io/PacROMSetSaver.java
@@ -16,6 +16,11 @@
 
 package io.github.thatgarlicdude.pacio.io;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
 /**
  * A class used to save universal Pac-Man ROM sets.
  * 
@@ -24,4 +29,121 @@ package io.github.thatgarlicdude.pacio.io;
  */
 public final class PacROMSetSaver implements PacROMSetFilenames {
 	
+	/**
+	 * The universal ROM set that is used to save on disk.
+	 */
+	private final PacROMSet pacROMSet;
+	
+	/**
+	 * The destination file path.
+	 */
+	private final String path;
+	
+	/**
+	 * Saves the universal ROM set to disk.
+	 * 
+	 * @throws IOException When the saving process fails.
+	 */
+	public final void saveROMSet() throws IOException {
+		FileOutputStream fos = new FileOutputStream(path);
+		ZipOutputStream zos = new ZipOutputStream(fos);
+		saveProgramROM(zos);
+		saveGraphicROM(zos);
+		saveColorROM(zos);
+		savePaletteROM(zos);
+		saveSoundROM(zos);
+		saveMysteryROM(zos);
+		zos.close();
+		fos.close();
+	}
+	
+	/**
+	 * Saves a specific ROM entry in the universal ROM set ZIP file.
+	 * 
+	 * @param zos The ZipOutputStream.
+	 * @param name The name of the ZIP file entry.
+	 * @param data The byte array that will be written on the ZIP file
+	 * entry.
+	 * @throws IOException When the saving process fails.
+	 */
+	private final void saveROM(
+			final ZipOutputStream zos,
+			final String name,
+			final byte[] data) throws IOException {
+		ZipEntry zipEntry = new ZipEntry(name);
+		zos.putNextEntry(zipEntry);
+		zos.write(data);
+		zos.closeEntry();
+	}
+	
+	/**
+	 * Saves the program ROM entry in the universal ROM set ZIP file.
+	 * 
+	 * @param zos The ZipOutputStream.
+	 * @throws IOException When the saving process fails.
+	 */
+	private final void saveProgramROM(final ZipOutputStream zos)
+			throws IOException {
+		saveROM(zos, getProgramName(), pacROMSet.getProgramData());
+	}
+	
+	/**
+	 * Saves the graphic ROM entry in the universal ROM set ZIP file.
+	 * 
+	 * @param zos The ZipOutputStream.
+	 * @throws IOException When the saving process fails.
+	 */
+	private final void saveGraphicROM(final ZipOutputStream zos)
+			throws IOException {
+		saveROM(zos, getGraphicName(), pacROMSet.getGraphicData());
+	}
+	
+	/**
+	 * Saves the color ROM entry in the universal ROM set ZIP file.
+	 * 
+	 * @param zos The ZipOutputStream.
+	 * @throws IOException When the saving process fails.
+	 */
+	private final void saveColorROM(final ZipOutputStream zos)
+			throws IOException {
+		saveROM(zos, getColorName(), pacROMSet.getColorData());
+	}
+	
+	/**
+	 * Saves the palette ROM entry in the universal ROM set ZIP file.
+	 * 
+	 * @param zos The ZipOutputStream.
+	 * @throws IOException When the saving process fails.
+	 */
+	private final void savePaletteROM(final ZipOutputStream zos)
+			throws IOException {
+		saveROM(zos, getPaletteName(), pacROMSet.getPaletteData());
+	}
+	
+	/**
+	 * Saves the sound ROM entry in the universal ROM set ZIP file.
+	 * 
+	 * @param zos The ZipOutputStream.
+	 * @throws IOException When the saving process fails.
+	 */
+	private final void saveSoundROM(final ZipOutputStream zos)
+			throws IOException {
+		saveROM(zos, getSoundName(), pacROMSet.getSoundData());
+	}
+	
+	/**
+	 * Saves the mystery ROM entry in the universal ROM set ZIP file.
+	 * 
+	 * @param zos The ZipOutputStream.
+	 * @throws IOException When the saving process fails.
+	 */
+	private final void saveMysteryROM(final ZipOutputStream zos)
+			throws IOException {
+		saveROM(zos, getMysteryName(), pacROMSet.getMysteryData());
+	}
+	
+	public PacROMSetSaver(final PacROMSet pacROMSet, final String path) {
+		this.pacROMSet = pacROMSet;
+		this.path = path;
+	}
 }
