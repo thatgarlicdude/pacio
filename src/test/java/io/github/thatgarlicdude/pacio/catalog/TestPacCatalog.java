6247e7c3eb8c8ac4f72c92439ace9cd453ef04f0
thatgarlicdude <garlicdudedev@gmail.com>
2026-07-06 15:48:29 -0400

    Reworked on the documentation for TestPacCatalog.java

--- src/test/java/io/github/thatgarlicdude/pacio/catalog/TestPacCatalog.java
+++ src/test/java/io/github/thatgarlicdude/pacio/catalog/TestPacCatalog.java
@@ -19,100 +19,182 @@ package io.github.thatgarlicdude.pacio.catalog;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
+/*
+ * TODO: Perhaps this class needs to be reworked so it doesn't rely on
+ * subclasses that are using the non-abstracted PacCatalog classes. With
+ * the PacCatalog class turning to a different direction, like becoming
+ * something that can be saved to disk and loaded back to memory, this
+ * test class should be reworked somehow.
+ */
+
 /**
  * A test class of the PacCatalog class.
  * 
- * @version 1.0.0-alpha
+ * @since 1.0.0-alpha
  * @author GarlicDude
+ * @see PacCatalog
  */
 public abstract class TestPacCatalog {
 	
 	/**
-	 * Placeholder message for the assertList method.
+	 * Placeholder message for the {@link #assertList(String[])} method.
+	 * 
+	 * @since 1.0.0-alpha
+	 * @author GarlicDude
+	 * @see #assertList(String[])
 	 */
-	private static final String PLACEHOLDER_MESSAGE = "List index %d: %s";
+	private static final String PLACEHOLDER_MESSAGE =
+			"List index %d: %s";
 	
 	/**
-	 * The PacCatalog used for testing purposes.
+	 * The {@link PacCatalog} used for testing purposes.
+	 * 
+	 * @since 1.0.0-alpha
+	 * @author GarlicDude
 	 */
 	private final PacCatalog pacCatalog;
 	
 	/**
-	 * Asserts a list within the class's PacCatalog to check if it's not
-	 * null.
+	 * Asserts a list within the {@link #pacCatalog} to check if it's not
+	 * null, and makes sure its contents are not null either.
 	 * 
 	 * @param list The list the method wants to assert.
+	 * @since 1.0.0-alpha
+	 * @author GarlicDude
 	 */
-	private final void assertList(final String[] list) {
+	private final void assertList(
+			final String[] list) {
+		// Make sure the list isn't null.
 		assertNotNull(list);
+		// Go through each of the list's filenames.
 		for (int index = 0; index < list.length; index++) {
-			String message = String.format(
-					PLACEHOLDER_MESSAGE,
-					index,
-					list[index]);
-			assertNotNull(list[index]);
+			// Get the filename in the list.
+			final String filename = list[index];
+			// Format the test message with the index and filename.
+			final String message =
+					String.format(PLACEHOLDER_MESSAGE, index, filename);
+			// Make sure the filename isn't null either.
+			assertNotNull(filename);
+			// Print the test message.
 			System.out.println(message);
 		}
 	}
 	
 	/**
-	 * Tests the getProgramROMNames method in the PacCatalog class.
+	 * Tests the {@link PacCatalog#getProgramROMNames()} method.
+	 * 
+	 * @since 1.0.0-alpha
+	 * @author GarlicDude
+	 * @see PacCatalog#getProgramROMNames()
 	 */
 	@Test
 	public final void testGetProgramROMNames() {
-		String[] programROMNames = pacCatalog.getProgramROMNames();
+		// Get the array of program ROM filenames.
+		final String[] programROMNames =
+				pacCatalog.getProgramROMNames();
+		// Make sure the list and its contents are not null.
 		assertList(programROMNames);
+		// Because this is a cloned array, of course they're not the same.
+		assertNotSame(programROMNames, pacCatalog.programROMNames);
 	}
 	
 	/**
-	 * Tests the getGraphicROMNames method in the PacCatalog class.
+	 * Tests the {@link PacCatalog#getGraphicROMNames()} method.
+	 * 
+	 * @since 1.0.0-alpha
+	 * @author GarlicDude
+	 * @see PacCatalog#getGraphicROMNames()
 	 */
 	@Test
 	public final void testGetGraphicROMNames() {
-		String[] graphicROMNames = pacCatalog.getGraphicROMNames();
+		// Get the array of graphic ROM filenames.
+		final String[] graphicROMNames =
+				pacCatalog.getGraphicROMNames();
+		// Make sure the list and its contents are not null.
 		assertList(graphicROMNames);
+		// Because this is a cloned array, of course they're not the same.
+		assertNotSame(graphicROMNames, pacCatalog.graphicROMNames);
 	}
 	
 	/**
-	 * Tests the getColorROMNames method in the PacCatalog class.
+	 * Tests the {@link PacCatalog#getColorROMNames()} method.
+	 * 
+	 * @since 1.0.0-alpha
+	 * @author GarlicDude
+	 * @see PacCatalog#getColorROMNames()
 	 */
 	@Test
-	public final void testGetColorROMs() {
-		String[] colorROMNames = pacCatalog.getColorROMNames();
+	public final void testGetColorROMNames() {
+		// Get the array of color ROM filenames.
+		final String[] colorROMNames =
+				pacCatalog.getColorROMNames();
+		// Make sure the list and its contents are not null.
 		assertList(colorROMNames);
+		// Because this is a cloned array, of course they're not the same.
+		assertNotSame(colorROMNames, pacCatalog.colorROMNames);
 	}
 	
 	/**
-	 * Tests the getPaletteROMNames method in the PacCatalog class.
+	 * Tests the {@link PacCatalog#getPaletteROMNames()} method.
+	 * 
+	 * @since 1.0.0-alpha
+	 * @author GarlicDude
+	 * @see PacCatalog#getPaletteROMNames()
 	 */
 	@Test
 	public final void testGetPaletteROMNames() {
-		String[] paletteROMNames = pacCatalog.getPaletteROMNames();
+		// Get the array of palette ROM filenames.
+		final String[] paletteROMNames =
+				pacCatalog.getPaletteROMNames();
+		// Make sure the list and its contents are not null.
 		assertList(paletteROMNames);
+		// Because this is a cloned array, of course they're not the same.
+		assertNotSame(paletteROMNames, pacCatalog.paletteROMNames);
 	}
 	
 	/**
-	 * Tests the getSoundROMNames method in the PacCatalog class.
+	 * Tests the {@link PacCatalog#getSoundROMNames()} method.
+	 * 
+	 * @since 1.0.0-alpha
+	 * @author GarlicDude
+	 * @see PacCatalog#getSoundROMNames()
 	 */
 	@Test
 	public final void testGetSoundROMNames() {
-		String[] soundROMNames = pacCatalog.getSoundROMNames();
+		// Get the array of sound ROM filenames.
+		final String[] soundROMNames =
+				pacCatalog.getSoundROMNames();
+		// Make sure the list and its contents are not null.
 		assertList(soundROMNames);
+		// Because this is a cloned array, of course they're not the same.
+		assertNotSame(soundROMNames, pacCatalog.soundROMNames);
 	}
 	
 	/**
-	 * Tests the getMysteryROMNames method in the PacCatalog class.
+	 * Tests the {@link PacCatalog#getMysteryROMNames()} method.
+	 * 
+	 * @since 1.0.0-alpha
+	 * @author GarlicDude
+	 * @see PacCatalog#getMysteryROMNames()
 	 */
 	@Test
 	public final void testGetMysteryROMNames() {
-		String[] mysteryROMNames = pacCatalog.getMysteryROMNames();
+		// Get the array of mystery ROM filenames.
+		final String[] mysteryROMNames =
+				pacCatalog.getMysteryROMNames();
+		// Make sure the list and its contents are not null.
 		assertList(mysteryROMNames);
+		// Because this is a cloned array, of course they're not the same.
+		assertNotSame(mysteryROMNames, pacCatalog.mysteryROMNames);
 	}
 	
 	/**
-	 * Constructs an instance of this test class.
+	 * Constructs an instance of the {@link TestPacCatalog} class.
 	 * 
-	 * @param pacCatalog The type of PacCatalog used for testing purposes.
+	 * @param pacCatalog The type of {@link PacCatalog} used for testing
+	 * purposes.
+	 * @since 1.0.0-alpha
+	 * @author GarlicDude
 	 */
 	public TestPacCatalog(final PacCatalog pacCatalog) {
 		this.pacCatalog = pacCatalog;
