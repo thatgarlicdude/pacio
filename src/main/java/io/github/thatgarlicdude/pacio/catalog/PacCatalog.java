a056bd230f95ba83c6bf684c3808f09baa595cc3
thatgarlicdude <garlicdudedev@gmail.com>
2026-07-06 14:03:18 -0400

    Improved the documentation for PacCatalog.java

--- src/main/java/io/github/thatgarlicdude/pacio/catalog/PacCatalog.java
+++ src/main/java/io/github/thatgarlicdude/pacio/catalog/PacCatalog.java
@@ -16,114 +16,186 @@
 
 package io.github.thatgarlicdude.pacio.catalog;
 
+/*
+ * TODO: In the future, the PacCatalog class should be in its own separate
+ * library, PacCatalogs. I feel like there can be a lot more with the
+ * PacCatalog classes than it meets the eye.
+ */
+
 /**
  * An abstract class that represents as a categorized list of ROM file
- * names.
+ * names in a <i>Pac-Man</i> ROM set.
  * 
- * @version 1.0.0-alpha
+ * @since 1.0.0-alpha
  * @author GarlicDude
+ * @see PacCatalogManager
+ * @see PacManCatalog
+ * @see PuckManCatalog
+ * @see TestCatalog
  */
 public abstract class PacCatalog {
 	
 	/**
-	 * The list of program ROM filenames in the ROM set.
-	 */
-	private final String[] programROMNames;
-	
-	/**
-	 * The list of graphic ROM filenames in the ROM set.
-	 */
-	private final String[] graphicROMNames;
-	
-	/**
-	 * The list of color ROM filenames in the ROM set.
-	 */
-	private final String[] colorROMNames;
-	
-	/**
-	 * The list of palette ROM filenames in the ROM set.
-	 */
-	private final String[] paletteROMNames;
-	
-	/**
-	 * The list of sound ROM filenames in the ROM set.
-	 */
-	private final String[] soundROMNames;
-	
-	/**
-	 * The list of mystery ROM filenames in the ROM set.
-	 */
-	private final String[] mysteryROMNames;
-	
-	/**
-	 * Gets a copied list of the program ROM filenames in the catalog.
+	 * The list of program ROM filenames that are in a <i>Pac-Man</i>
+	 * ROM set.
 	 * 
-	 * @return The string array consisting of program ROM filenames.
+	 * @since 1.0.0-alpha
+	 * @author GarlicDude
+	 * @see #getProgramROMNames()
+	 */
+	protected final String[] programROMNames;
+	
+	/**
+	 * The list of graphic ROM filenames that are in a <i>Pac-Man</i>
+	 * ROM set.
+	 * 
+	 * @since 1.0.0-alpha
+	 * @author GarlicDude
+	 * @see #getGraphicROMNames()
+	 */
+	protected final String[] graphicROMNames;
+	
+	/**
+	 * The list of color ROM filenames that are in a <i>Pac-Man</i> ROM
+	 * set.
+	 * 
+	 * @since 1.0.0-alpha
+	 * @author GarlicDude
+	 * @see #getColorROMNames()
+	 */
+	protected final String[] colorROMNames;
+	
+	/**
+	 * The list of palette ROM filenames that are in a <i>Pac-Man</i>
+	 * ROM set.
+	 * 
+	 * @since 1.0.0-alpha
+	 * @author GarlicDude
+	 * @see #getPaletteROMNames()
+	 */
+	protected final String[] paletteROMNames;
+	
+	/**
+	 * The list of sound ROM filenames that are in a <i>Pac-Man</i> ROM
+	 * set.
+	 * 
+	 * @since 1.0.0-alpha
+	 * @author GarlicDude
+	 * @see #getSoundROMNames()
+	 */
+	protected final String[] soundROMNames;
+	
+	/**
+	 * The list of mystery ROM filenames that are in a <i>Pac-Man</i>
+	 * ROM set.
+	 * 
+	 * @since 1.0.0-alpha
+	 * @author GarlicDude
+	 * @see #getMysteryROMNames()
+	 */
+	protected final String[] mysteryROMNames;
+	
+	/**
+	 * Gets a cloned array of the {@link #programROMNames} array, which is
+	 * a list of program ROM filenames that are in a <i>Pac-Man</i> ROM
+	 * set.
+	 * 
+	 * @return A cloned array of the {@link #programROMNames} array.
+	 * @since 1.0.0-alpha
+	 * @author GarlicDude
+	 * @see #programROMNames
 	 */
 	public final String[] getProgramROMNames() {
 		return programROMNames.clone();
 	}
 	
 	/**
-	 * Gets a copied list of the graphic ROM filenames in the catalog.
+	 * Gets a cloned array of the {@link #graphicROMNames} array, which is
+	 * a list of graphic ROM filenames that are in a <i>Pac-Man</i> ROM
+	 * set.
 	 * 
-	 * @return The string array consisting of graphic ROM filenames.
+	 * @return A cloned array of the {@link #graphicROMNames} array.
+	 * @since 1.0.0-alpha
+	 * @author GarlicDude
+	 * @see #graphicROMNames
 	 */
 	public final String[] getGraphicROMNames() {
 		return graphicROMNames.clone();
 	}
 	
 	/**
-	 * Gets a copied list of the color ROM filenames in the catalog.
+	 * Gets a cloned array of the {@link #colorROMNames} array, which is
+	 * a list of color ROM filenames that are in a <i>Pac-Man</i> ROM
+	 * set.
 	 * 
-	 * @return The string array consisting of color ROM filenames.
+	 * @return A cloned array of the {@link #colorROMNames} array.
+	 * @since 1.0.0-alpha
+	 * @author GarlicDude
+	 * @see #colorROMNames
 	 */
 	public final String[] getColorROMNames() {
 		return colorROMNames.clone();
 	}
 	
 	/**
-	 * Gets a copied list of the palette ROM filenames in the catalog.
+	 * Gets a cloned array of the {@link #paletteROMNames} array, which is
+	 * a list of palette ROM filenames that are in a <i>Pac-Man</i> ROM
+	 * set.
 	 * 
-	 * @return The string array consisting of palette ROM filenames.
+	 * @return A cloned array of the {@link #paletteROMNames} array.
+	 * @since 1.0.0-alpha
+	 * @author GarlicDude
+	 * @see #paletteROMNames
 	 */
 	public final String[] getPaletteROMNames() {
 		return paletteROMNames.clone();
 	}
 	
 	/**
-	 * Gets a copied list of the sound ROM filenames in the catalog.
+	 * Gets a cloned array of the {@link #soundROMNames} array, which is
+	 * a list of sound ROM filenames that are in a <i>Pac-Man</i> ROM
+	 * set.
 	 * 
-	 * @return The string array consisting of sound ROM filenames.
+	 * @return A cloned array of the {@link #soundROMNames} array.
+	 * @since 1.0.0-alpha
+	 * @author GarlicDude
+	 * @see #soundROMNames
 	 */
 	public final String[] getSoundROMNames() {
 		return soundROMNames.clone();
 	}
 	
 	/**
-	 * Gets a copied list of the mystery ROM filenames in the catalog.
+	 * Gets a cloned array of the {@link #mysteryROMNames} array, which is
+	 * a list of mystery ROM filenames that are in a <i>Pac-Man</i> ROM
+	 * set.
 	 * 
-	 * @return The string array consisting of mystery ROM filenames.
+	 * @return A cloned array of the {@link #mysteryROMNames} array.
+	 * @since 1.0.0-alpha
+	 * @author GarlicDude
+	 * @see #colorROMNames
 	 */
 	public final String[] getMysteryROMNames() {
 		return mysteryROMNames.clone();
 	}
 	
 	/**
-	 * Constructs an instance of the PacCatalog.
+	 * Constructs an instance of the {@link PacCatalog} class.
 	 * 
-	 * @param programROMNames The list of program ROM filenames in the
-	 * ROM set.
-	 * @param graphicROMNames The list of graphic ROM filenames in the
-	 * ROM set.
-	 * @param colorROMNames The list of color ROM filenames in the ROM
-	 * set.
-	 * @param paletteROMNames The list of palette ROM filenames in the
-	 * ROM set.
-	 * @param soundROMNames The list of sound ROM filenames in the ROM
-	 * set.
-	 * @param mysteryROMNames The list of mystery ROM filenames in the
-	 * ROM set.
+	 * @param programROMNames The list of program ROM filenames that are
+	 * in a <i>Pac-Man</i> ROM set.
+	 * @param graphicROMNames The list of graphic ROM filenames that are
+	 * in a <i>Pac-Man</i> ROM set.
+	 * @param colorROMNames The list of color ROM filenames that are in a
+	 * <i>Pac-Man</i> ROM set.
+	 * @param paletteROMNames The list of palette ROM filenames that are
+	 * in a <i>Pac-Man</i> ROM set.
+	 * @param soundROMNames The list of sound ROM filenames that are in a
+	 * <i>Pac-Man</i> ROM set.
+	 * @param mysteryROMNames The list of mystery ROM filenames that are
+	 * in a <i>Pac-Man</i> ROM set.
+	 * @since 1.0.0-alpha
+	 * @author GarlicDude
 	 */
 	protected PacCatalog(
 			final String[] programROMNames,
